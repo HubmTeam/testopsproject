ed8ee0d0796aed174134c04df2aaa733d22a47e5
HUBMATRIX-T-04 <sumanayadav1144@gmail.com>
2018-06-06 14:43:12 +0530

    My Commit message

---
+++ app/src/main/java/opsconnect_hubmatrix/oconnect/com/testopsproject/LoginOpsConnect.java
@@ -0,0 +1,19 @@
+package opsconnect_hubmatrix.oconnect.com.testopsproject;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class LoginOpsConnect extends AppCompatActivity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		TextView txt = findViewById(R.id.txt);
+		txt.setText("Welcome Login Ops Connect");
+		Toast.makeText(getApplicationContext(), " Hello Login Ops Connect ", Toast.LENGTH_LONG).show();
+	}
+}
