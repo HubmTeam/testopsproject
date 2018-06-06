d5e8881fe7a9484325bc65034e2445e134a7b0d1
HUBMATRIX-T-04 <sumanayadav1144@gmail.com>
2018-06-06 14:12:37 +0530

    My Commit message

---
+++ app/src/main/java/opsconnect_hubmatrix/oconnect/com/testopsproject/MainActivity.java
@@ -0,0 +1,13 @@
+package opsconnect_hubmatrix.oconnect.com.testopsproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
